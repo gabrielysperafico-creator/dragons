a660744ebf81683ca8979ed76e76b12cc3d994f1
gabrielysperafico-creator <gabriely.sperafico@escola.pr.gov.br>
2026-08-16 22:15:37 -0300

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showInfo(Image) {
  const article = Image.parentElement;
 
  const Info = article.querySelectorAll(".dragon-info");
- alert("encontrei" + Info.length + "elementos");
+ 
 
  Info.forEach(element  => {
     element.style.display = "block";
